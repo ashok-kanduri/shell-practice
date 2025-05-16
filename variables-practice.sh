5bc8ae96dbff1d61e89e085cad55c6f2da9ed2d9
ashokkumar <ashkanduri@gmail.com>
2025-05-17 00:22:51 +0530

    variables-practice

---
+++ variables-practice.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "all variables passed through the script: $@"
+echo "number of variables: $#"
+echo "script name: $0"
+echo "current directory: $PWD"
+echo "user running this script: $USER"
+echo "home directory of user: $HOME"
+echo "PID of the script: $$"
