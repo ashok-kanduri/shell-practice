689ad1ac9291df1c6e088648162811eb9d27654c
ashokkumar <ashkanduri@gmail.com>
2025-05-16 17:39:46 +0530

    my view

--- hello-world.sh
+++ hello-world.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo "hi this is ashok, i came to a realisation that life is of no point, and life in universe might be very rare, but life on earth isnt rare. "
+echo "hi this is ashok, i came to a realisation that life is of no point, and life in universe might be very rare, but life on earth isnt rare."
+echo "have you ever thought what is life about and why do we go through all this in life?"
