1515267c382336ca288a07c4d8cb54b24418444e
ashokkumar <ashkanduri@gmail.com>
2025-05-17 00:47:34 +0530

    variables-practice

---
+++ mysql.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USERID=(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: please run the script with root access"
+    exit 1 
+else
+    echo "you are running with root access"
+fi
+
+dnf install mysql -y
+if [ $? -eq 0 ]
+then 
+    echo "installing mysql is SUCCESS...."
+else
+echo "installing mysql is FAILURE...."
+exit 1
+fi
