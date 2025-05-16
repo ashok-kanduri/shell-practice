e01b2a1b52b02de1a312b36858c260900639835a
ashokkumar <ashkanduri@gmail.com>
2025-05-17 01:47:03 +0530

    variables-practice

--- mysql.sh
+++ mysql.sh
@@ -25,3 +25,18 @@ then
 else
     echo "mysql is already installed..."
 fi
+
+dnf list installed python3
+if [ $? -ne 0 ]
+then 
+    echo "pyhton is not installed. going to install it"
+    dnf install python3 -y
+    if [ $? -ne 0 ]
+        echo "installing python3 is SUCCESS"
+    else
+        echo "installing python3 is FAILURE"
+        exit 1
+    fi
+else
+    echo "python3 is already installed"
+fi    
