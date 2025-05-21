75f1d681fe839c1402a3a5400c05e27b9326e99f
ashokkumar <ashkanduri@gmail.com>
2025-05-21 21:42:13 +0530

    please store

--- 07.redirections.sh
+++ 07.redirections.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f2)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 # &>> --> shows detail output in logs file
