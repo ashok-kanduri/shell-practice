1827653eac728ab29d4c46d86b536bc895635101
ashokkumar <ashkanduri@gmail.com>
2025-05-21 20:20:31 +0530

    please store

--- 03-mysql.sh
+++ 03-mysql.sh
@@ -26,34 +26,4 @@ else
     echo "mysql is already installed..."
 fi
 
-dnf list installed python3
-if [ $? -ne 0 ]
-then 
-    echo "pyhton is not installed. going to install it"
-    dnf install python3 -y
-    if [ $? -ne 0 ]
-    then
-        echo "installing python3 is SUCCESS"
-    else
-        echo "installing python3 is FAILURE"
-        exit 1
-    fi
-else
-    echo "python3 is already installed"
-fi    
 
-dnf list installed nginx
-if [ $? -ne 0 ]
-then 
-    echo "nginx is not installed. going to install it"
-    dnf install nginx -y
-    if [ $? -ne 0 ]
-    then
-        echo "installing nginx is SUCCESS"
-    else
-        echo "installing nginx is FAILURE"
-        exit 1
-    fi
-else
-    echo "nginx is already installed"
-fi    
