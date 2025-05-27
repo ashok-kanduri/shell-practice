ffd3bc3564805d322844b416d66de0291b693e2b
ashokkumar <ashkanduri@gmail.com>
2025-05-27 17:48:49 +0530

    make the changes

--- 09-while-loop.sh
+++ 09-while-loop.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-ASHOK="/home/ec2-user/shell-practice/ashok"
+SOURCE_DIR=/home/ec2-user/app-logs
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 
 while IFS= read -r line
 do
-  echo %line
-done < $ASHOK
+  echo "Deleting the file: $line"
+  rm -rf $line
+done <<< $FILES_TO_DELETE
+
+echo "Script executed successfully"
